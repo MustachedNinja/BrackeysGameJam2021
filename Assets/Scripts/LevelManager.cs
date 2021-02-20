0175b74060aadd552469ec8e5e2b4a10ea83e1fd
MustachedNinja <kostya.yatsuk9@live.com>
2021-02-20 00:33:43 -0800

    Add LevelComplete UI

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadNextLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ReturnMainMenu() {
+        SceneManager.LoadScene(0);
+    }
+}
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,6 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+
+    [SerializeField]
+    private GameObject mainMenuPanel = null;
+    [SerializeField]
+    private GameObject levelSelectPanel = null;
+    void Start() {
+        mainMenuPanel.SetActive(true);
+        levelSelectPanel.SetActive(false);    
+    }
+
     public void PlayLevel(int levelIndex) {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + levelIndex);
     }
